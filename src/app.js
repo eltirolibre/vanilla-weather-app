9d92b50aac9d6edbd2857c92fc6c5d7c64690972
eltirolibre <109189557+eltirolibre@users.noreply.github.com>
2022-07-31 10:22:47 -0400

    Began to add the daily forecast functions
    Added API with latitude and longitude.

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tues", "Wed", "Thu"];
@@ -49,6 +49,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 //
+function getForeCast(coordinates) {
+  let apiKey = "7882cc418788de0e994c37cdce235ed9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -71,6 +76,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForeCast(response.data.coord);
 }
 
 function search(city) {
@@ -114,4 +121,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("Toronto");
-displayForecast();
